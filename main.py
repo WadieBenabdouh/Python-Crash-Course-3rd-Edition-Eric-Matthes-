5f6e4831b2fc479f3a3a3d339ac384295276ede9
WadieBenabdouh <wadie.trade@gmail.com>
2024-03-14 02:15:28 +0000

    task transferred to second laptop

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# 4.1 Think of at least three kinds of your favorite pizza. Store these pizza names in a list, and then use a for loop to print the name of each pizza
-favorite_pizzas = ["Sea-Food Pizza", "Vegetarian Pizza", "Mushroom Pizza"]
-for pizza in favorite_pizzas:
-    print(f'I like {pizza}.')
-print("I really love Pizza.")
+numbers = list(range(0, 101))
 
-# 4-2. Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop toprint out the name of each animal.
-animals = ["Tiger", "Leopard", "Lynx"]
-for animal in animals:
-    print(animal)
-print(f'{animals[0]}, is strong and aggresive.')
-print(f'{animals[1]}, is agile and spiritual.')
-print(f'{animals[2]}, is fluffy, yet a predator.')
+oddNumbers = [number for number in numbers if number % 2 != 0]
+print(oddNumbers)
+
+evenNumbers = [number for number in numbers if number % 2 == 0]
+print(evenNumbers)
+
+for number in numbers:
+    number * 10
+multiples_three = [number]
+
+# fix this above 
